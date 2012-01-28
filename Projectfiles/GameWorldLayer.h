e45527a4af828622f7db993793ea371240ffbc1d
Kyle Reczek <savethejets1@gmail.com>
2012-01-28 09:46:14 -0700

    A few updates to the code.
    - Physics work now, player will fall to the ground when the game starts
    - Added GameWorldLayer that holds the three red/blue/yellow CCLayers
    - Added some sweet shades to the player.png

---
+++ Projectfiles/GameWorldLayer.h
@@ -0,0 +1,12 @@
+//
+//  Created by kylereczek on 12-01-28.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import <Foundation/Foundation.h>
+
+
+@interface GameWorldLayer : NSObject
+@end
---
+++ Projectfiles/GameWorldLayer.m
@@ -0,0 +1,15 @@
+//
+//  Created by kylereczek on 12-01-28.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import "GameWorldLayer.h"
+
+
+@implementation GameWorldLayer {
+
+}
+
+@end
