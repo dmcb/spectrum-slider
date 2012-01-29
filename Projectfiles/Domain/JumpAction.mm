d30a4b8d6d6378f8b2f58e763862320f3e96cfbf
Kyle Reczek <savethejets1@gmail.com>
2012-01-28 17:23:14 -0700

    Initial Stab at Jumping.
    - jumping needs some work still.

---
+++ Projectfiles/Domain/JumpAction.mm
@@ -0,0 +1,15 @@
+//
+//  Created by kylereczek on 12-01-28.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import "JumpAction.h"
+
+
+@implementation JumpAction {
+
+}
+
+@end
